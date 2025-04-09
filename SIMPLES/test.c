e6f96f43cb33b85b1a3878138e4ddb5f09e7a5a3
HeriklysBernardes <106178773+HeriklysBernardes@users.noreply.github.com>
2025-04-09 19:54:23 +0000

    modified:   SIMPLES/build/jar/simples-compiler.jar 	modified:   SIMPLES/src/SimplesLexer.java 	modified:   SIMPLES/test.c

--- SIMPLES/test.c
+++ SIMPLES/test.c
@@ -8,7 +8,10 @@ float d = d+b;
 float g = c+a;
 float f = f+a;
 float r = r+b;
+<<<<<<< HEAD
 float p = p+r;
+=======
+>>>>>>> 44dad09 (	modified:   SIMPLES/build/jar/simples-compiler.jar)
 b = a+b;
 
 return 0; 
